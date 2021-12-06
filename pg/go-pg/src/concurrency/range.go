4bc6542432f73dab7ccded4ca74aa5a4be1d99f8
Arthur Christoph <achristoph@gmail.com>
2021-12-05 22:22:29 -0800

    .

---
+++ pg/go-pg/src/concurrency/range.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Money struct {
+	amount int
+	year   int
+}
+
+func sendMoney(parent chan Money) {
+
+	for i := 0; i <= 18; i++ {
+		parent <- Money{5000, i}
+	}
+	close(parent)
+}
+
+func main() {
+	money := make(chan Money)
+
+	go sendMoney(money)
+
+	// for i := 0; i <= 18; i++ {
+	// 	kidMoney := <-money
+	// 	fmt.Printf("Money received by kid in year %d : %d\n", kidMoney.year, kidMoney.amount)
+	// }
+
+	for kidMoney := range money {
+		fmt.Printf("Money received by kid in year %d : %d\n", kidMoney.year, kidMoney.amount)
+	}
+}
